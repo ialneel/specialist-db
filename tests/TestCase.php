b34bb252622d39439414721858d022ac6d475d2e
ialneel <alneelibrahim01@gmail.com>
2025-07-23 19:10:05 +0300

    TestCase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,12 +7,4 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
-
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        // Stop Laravel from trying to read public/build/manifest.json in tests
-        $this->withoutVite();
-    }
 }
