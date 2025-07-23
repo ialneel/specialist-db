11745fbefc9165bd28a9a62be1093623e0f0cd29
ialneel <alneelibrahim01@gmail.com>
2025-07-23 18:59:42 +0300

    test: call withoutVite() to skip Vite manifest in tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,5 +6,13 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    use CreatesApplication;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        // Stop Laravel from trying to read public/build/manifest.json in tests
+        $this->withoutVite();
+    }
 }
